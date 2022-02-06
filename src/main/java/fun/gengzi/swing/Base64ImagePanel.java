283b0bbea14573c8eaa9476e43469c47959b2241
耿子 <1164014750@qq.com>
2022-02-06 16:08:01 +0800

    [version1.01] <business> (1) change Base64Image panel style
    <dependent>
    (1) nothing

--- src/main/java/fun/gengzi/swing/Base64ImagePanel.java
+++ src/main/java/fun/gengzi/swing/Base64ImagePanel.java
@@ -4,46 +4,33 @@ package fun.gengzi.swing;
 import cn.hutool.core.img.ImgUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.FileNameUtil;
-import cn.hutool.core.util.ReUtil;
-import cn.hutool.core.util.URLUtil;
-import com.intellij.ide.DataManager;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx;
-import com.intellij.openapi.fileEditor.impl.EditorWindow;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.ui.JBPopupMenu;
+import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.ui.components.JBScrollPane;
-import com.intellij.ui.components.JBViewport;
+import com.intellij.ui.components.*;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.util.containers.ContainerUtil;
+import fun.gengzi.constant.GlobalConstant;
 import fun.gengzi.imgeservice.ImageFilePathProcess;
 import fun.gengzi.message.NotficationMsg;
 import fun.gengzi.utils.I18nBundle;
-import groovy.ui.text.TextEditor;
+import fun.gengzi.utils.UiRefreshThreadUtils;
 import lombok.Getter;
 import lombok.SneakyThrows;
-import org.intellij.images.editor.ImageDocument;
-import org.intellij.images.editor.impl.ImageEditorImpl;
-import org.intellij.images.ui.ImageComponent;
-import org.intellij.images.vfs.IfsUtil;
 import org.jdesktop.swingx.JXButton;
-import org.jdesktop.swingx.JXImageView;
-import org.jdesktop.swingx.JXLabel;
 import org.jdesktop.swingx.JXPanel;
-import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriter;
 import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.List;
 
 
 /**
@@ -54,20 +41,26 @@ import java.io.IOException;
  */
 @Getter
 public class Base64ImagePanel extends JXPanel implements ImageFilePathProcess {
+    // 默认的图片UI
     private DefaultImageEditorUI jxImageView;
-    private JTextArea base64Editor;
-    private JBScrollPane jbScrollPane;
     private JXPanel jxPanel;
+    private JBTextArea base64Editor;
+    private JBScrollPane jbScrollPane;
     private JXPanel lastPanel;
-    private JSplitPane editorPanel;
+    private JXPanel topPanel;
+    private JXPanel editorPanel;
     private static final Logger LOG = Logger.getInstance(Base64ImagePanel.class);
     private static final int BLACK = new Color(0, 0, 0).getRGB();
     private static final int WHITE = new Color(232, 229, 229).getRGB();
     private double SW = 192;
-    private JTextField imageThreshold;
-    private JXLabel thresholdLabel;
-    private JXButton okButton;
+    private JXButton toBase64Button;
     private String imgPath;
+    private Runnable runnable;
+    private ComboBox<String> imgTypeComboBox;
+    private JBTextField imageTypeTextField;
+    // 支持的图片格式
+    private static final List<String> IMAGE_TYPE = ContainerUtil.immutableList("png", "ico", "bmp", "gif", "jpg", "svg");
+    private String[] arr = {};
 
     /**
      * 初始化面板
@@ -81,32 +74,82 @@ public class Base64ImagePanel extends JXPanel implements ImageFilePathProcess {
         initCompant();
         // 监听器
         addAllActionListener();
+        // 异步刷新UI任务
+        this.runnable = () -> {
+            showImage();
+        };
     }
 
+
     private void initCompant() throws IOException {
-
-        base64Editor = new JTextArea();
+        // 顶部panel
+        topPanel = new JXPanel(new FlowLayout());
+        imgTypeComboBox = new ComboBox(IMAGE_TYPE.toArray(arr));
+        imgTypeComboBox.setSelectedIndex(0);
+        imageTypeTextField = new JBTextField();
+        imageTypeTextField.setText(tobase64ImageTypeStr(imgTypeComboBox.getSelectedItem().toString()));
+        topPanel.add(imgTypeComboBox);
+        topPanel.add(imageTypeTextField);
+        topPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        // 中部
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(2, 1);
+        editorPanel = new JXPanel(gridLayoutManager);
+        base64Editor = new JBTextArea(50, 50);
+        base64Editor.setSize(new Dimension(500, this.getHeight()));
         jbScrollPane = new JBScrollPane(base64Editor);
-        base64Editor.setMinimumSize(new Dimension(100, this.getHeight()));
-        lastPanel = new JXPanel();
-        thresholdLabel = new JXLabel();
-        thresholdLabel.setText(I18nBundle.message(I18nBundle.Key.PIXELIMAGEPANEL_THRESHOLD));
-        okButton = new JXButton();
-        okButton.setText("Base64 to Image");
-        lastPanel.setLayout(new BorderLayout());
         jxImageView = new DefaultImageEditorUI(null);
-        jxImageView.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
-        imageThreshold = new JTextField();
-        imageThreshold.setToolTipText(I18nBundle.message(I18nBundle.Key.PIXELIMAGEPANEL_IMGPATH_TOOLTIPTEXT));
-        lastPanel.add(okButton, BorderLayout.LINE_START);
-
-        editorPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jbScrollPane, jxImageView);
-        this.add(lastPanel, BorderLayout.PAGE_END);
+        GridConstraints gridConstraints = new GridConstraints();
+        gridConstraints.setRow(0);
+        gridConstraints.setColumn(0);
+        gridConstraints.setFill(GridConstraints.FILL_BOTH);
+        editorPanel.add(jbScrollPane, gridConstraints);
+        jbScrollPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+        GridConstraints gridConstraints1 = new GridConstraints();
+        gridConstraints1.setRow(1);
+        gridConstraints1.setColumn(0);
+        gridConstraints1.setFill(GridConstraints.FILL_BOTH);
+        editorPanel.add(jxImageView, gridConstraints1);
+        jxImageView.setBorder(new LineBorder(new Color(0, 0, 0)));
+        // 尾部
+        lastPanel = new JXPanel();
+        toBase64Button = new JXButton();
+        toBase64Button.setText(I18nBundle.message(I18nBundle.Key.BASE64IMAGEPANEL_TOBASE64BUTTON_TEXT));
+        lastPanel.setLayout(new BorderLayout());
+        lastPanel.add(toBase64Button, BorderLayout.LINE_START);
+        // 总布局
+        this.add(topPanel, BorderLayout.PAGE_START);
         this.add(editorPanel, BorderLayout.CENTER);
+        this.add(lastPanel, BorderLayout.PAGE_END);
+
+    }
+
+    /**
+     * @param imgType
+     * @return
+     */
+    private String tobase64ImageTypeStr(String imgType) {
+        return String.format(GlobalConstant.BASE64_IMAGETYPE, imgType);
     }
 
     private void addAllActionListener() {
-        okButton.addActionListener(e -> {
+
+        imgTypeComboBox.addActionListener(e -> {
+            // 获取现在选择的选项
+            String selectedItem = (String) imgTypeComboBox.getSelectedItem();
+            imageTypeTextField.setText(tobase64ImageTypeStr(selectedItem));
+        });
+
+
+
+
+
+
+
+
+
+
+
+        toBase64Button.addActionListener(e -> {
             String x = ";base64,";
             // base64 转 image
             String content = base64Editor.getText();
@@ -114,18 +157,35 @@ public class Base64ImagePanel extends JXPanel implements ImageFilePathProcess {
             String[] split = content.split(x);
             String imgtype = split[0].replace("data:image/", "");
             BufferedImage bufferedImage = ImgUtil.toImage(split[split.length - 1]);
-            ImageWriter writer = ImgUtil.getWriter(bufferedImage, imgtype);
-            try {
-                ImgUtil.write(bufferedImage, imgtype, new FileOutputStream(new File("C:\\Users\\Administrator\\Desktop\\wallhaven-e7rje8.jpg")));
-            } catch (FileNotFoundException fileNotFoundException) {
-                fileNotFoundException.printStackTrace();
-            }
-
-
-            JBPopupMenu jbPopupMenu = new JBPopupMenu();
-            jbPopupMenu.setPreferredSize(new Dimension(200, 200));
-            jbPopupMenu.add("hahaha");
-            this.add(jbPopupMenu, BorderLayout.CENTER);
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
+//            try {
+//                ImageIO.write(bufferedImage, imgtype, out);
+//            } catch (IOException ioException) {
+//                ioException.printStackTrace();
+//            }
+//            LightVirtualFile virtualFile = new LightVirtualFile();
+//            ApplicationManager.getApplication().runWriteAction(new Runnable() {
+//                @SneakyThrows
+//                @Override
+//                public void run() {
+//                    virtualFile.setWritable(true);
+//                    virtualFile.setBinaryContent(out.toByteArray());
+//                }
+//            });
+            jxImageView.showImage(bufferedImage);
+//
+//            ImageWriter writer = ImgUtil.getWriter(bufferedImage, imgtype);
+//            try {
+//                ImgUtil.write(bufferedImage, imgtype, new FileOutputStream(new File("C:\\Users\\Administrator\\Desktop\\wallhaven-e7rje8.jpg")));
+//            } catch (FileNotFoundException fileNotFoundException) {
+//                fileNotFoundException.printStackTrace();
+//            }
+//
+//
+//            JBPopupMenu jbPopupMenu = new JBPopupMenu();
+//            jbPopupMenu.setPreferredSize(new Dimension(200, 200));
+//            jbPopupMenu.add("hahaha");
+//            this.add(jbPopupMenu, BorderLayout.CENTER);
         });
     }
 
@@ -138,28 +198,18 @@ public class Base64ImagePanel extends JXPanel implements ImageFilePathProcess {
     @SneakyThrows
     @Override
     public void process(String imgPath) {
-//        ProjectManager instance = ProjectManager.getInstance();
-//        Project defaultProject = instance.getDefaultProject();
-//        @NotNull Project[] openProjects = instance.getOpenProjects();
-//        VirtualFile fileByIoFile = LocalFileSystem.getInstance().findFileByIoFile(new File(imgPath));
-//        FileEditorManagerEx fileEditorManager = FileEditorManagerEx.getInstanceEx(openProjects[0]);
-////        VirtualFileManager instance1 = VirtualFileManager.getInstance();
-////        VirtualFile picture = instance1.findFileByUrl(imgPath);
-//        fileEditorManager.openFile(fileByIoFile, true);
-//
-//        EditorWindow currentWindow = fileEditorManager.getCurrentWindow();
-//
-//        JBViewport jbViewport = new JBViewport();
-//
-//
-//        ImageEditorImpl imageEditor = new ImageEditorImpl(openProjects[0],fileByIoFile);
-
-
         boolean blank = NotficationMsg.isBlank(imgPath);
         if (blank) {
             return;
         }
         this.imgPath = imgPath;
+        // 异步执行
+        UiRefreshThreadUtils.instance(runnable).start();
+    }
+
+
+    @SneakyThrows
+    private void showImage() {
         File file = FileUtil.file(imgPath);
         // 解析文件名称
         String extName = FileNameUtil.extName(file);
@@ -167,15 +217,12 @@ public class Base64ImagePanel extends JXPanel implements ImageFilePathProcess {
         String base64 = ImgUtil.toBase64DataUri(src, extName);
         System.out.println(base64);
         base64Editor.setText(base64);
-        base64Editor.setFont(new Font("Consolas", Font.PLAIN, 16));
+        base64Editor.setFont(new Font("Consolas", Font.PLAIN, 5));
         base64Editor.setLineWrap(true);
         base64Editor.setWrapStyleWord(true);
-        base64Editor.setPreferredSize(new Dimension(200, this.jxPanel.getHeight()));
         VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(new File(imgPath));
         jxImageView.showImage(virtualFile);
-
-//        jxImageView.setPreferredSize(new Dimension(this.jxPanel.getWidth() / 2 - 30, this.jxPanel.getHeight()));
-//        jxImageView.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        jxImageView.updateUI();
     }
 
 }
